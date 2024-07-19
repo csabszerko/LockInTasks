df11d49bc42b25a936f9c5d209b606614f0eed6e
csabszerko <kiss.csaba.david@gmail.com>
2024-07-19 18:29:16 +0200

    Initial Commit

---
+++ LockInTasks/LockInTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockInTasksApp.swift
+//  LockInTasks
+//
+//  Created by Csaba Kiss on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LockInTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
