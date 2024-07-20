e2acc513e9e4842708681af92cc8a6a285952f67
csabszerko <kiss.csaba.david@gmail.com>
2024-07-20 22:18:22 +0200

    working checkboxes

---
+++ LockInTasks/Views/CustomCheckBoxView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCheckBoxView.swift
+//  LockInTasks
+//
+//  Created by Csaba Kiss on 20/07/2024.
+//
+
+import SwiftUI
+
+struct CustomCheckBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomCheckBoxView()
+}
